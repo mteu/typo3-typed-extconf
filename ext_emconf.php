ebbd2b4ef50821561f912fee7b0bbef3e565387f
Martin Adler <mteu@mailbox.org>
2025-11-25 09:36:28 +0100

    [TASK] Remove v12 from ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'mteu@mailbox.org',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.31-13.4.99',
+            'typo3' => '13.4.18-13.4.99',
             'php' => '8.2.0-8.5.99',
         ],
     ],
