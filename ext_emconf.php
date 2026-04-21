962113a9cc51dc1b7e0e71f539c5f05d4b7bf623
Martin Adler <mteu@mailbox.org>
2026-04-21 12:42:56 +0200

    [TASK] Update ext_emconf to properly reflect current TYPO3 v14.3 LTS support

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,7 +27,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'mteu@mailbox.org',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.4.18-14.2.99',
+            'typo3' => '13.4.18-14.3.99',
             'php' => '8.2.0-8.5.99',
         ],
     ],
